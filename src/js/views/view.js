da854f7637d18cdf4a7004bc4ff828ae75488ac9
Ram Sarfian <39989110+ramsar7002@users.noreply.github.com>
2021-08-15 14:46:53 +0300

    Create view.js

---
+++ src/js/views/view.js
@@ -0,0 +1,59 @@
+import icons from 'url:../../img/icons.svg';
+
+export default class View {
+  _data;
+  render(data) {
+    this._data = data;
+
+    const markup = this._generateMarkup();
+    console.log(markup);
+
+    this._clear();
+    this._parentElement.insertAdjacentHTML('afterbegin', markup);
+  }
+
+  _clear() {
+    this._parentElement.innerHTML = '';
+  }
+  renderSpinner = () => {
+    const markup = `<div class="spinner">
+              <svg>
+                <use href="${icons}#icon-loader"></use>
+              </svg>
+            </div>`;
+    this._clear();
+    this._parentElement.insertAdjacentHTML('afterbegin', markup);
+  };
+
+  renderError(errMessage = this._errorMessage) {
+    const markup = `
+        <div class="error">
+          <div>
+            <svg>
+              <use href="${icons}.svg#icon-alert-triangle"></use>
+            </svg>
+          </div>
+          <p>${errMessage}</p>
+        </div>;
+      `;
+
+    this._clear();
+    this._parentElement.insertAdjacentHTML('afterbegin', markup);
+  }
+
+  renderMessage(errMessage = this._message) {
+    const markup = `
+        <div class="message">
+          <div>
+            <svg>
+              <use href="${icons}.svg#icon-smile"></use>
+            </svg>
+          </div>
+          <p>${errMessage}</p>
+        </div>;
+      `;
+
+    this._clear();
+    this._parentElement.insertAdjacentHTML('afterbegin', markup);
+  }
+}
