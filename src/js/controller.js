c3e412a777f1b4284a2a4975d49b818f99e1f11e
Ram Sarfian <39989110+ramsar7002@users.noreply.github.com>
2021-08-15 11:56:18 +0300

    Update controller.js

--- src/js/controller.js
+++ src/js/controller.js
@@ -6,14 +6,6 @@ import 'regenerator-runtime/runtime';
 const recipeContainer = document.querySelector('.recipe');
 const searchResultsContainer = document.querySelector('.search-results');
 
-const timeout = function (s) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
-      reject(new Error(`Request took too long! Timeout after ${s} second`));
-    }, s * 1000);
-  });
-};
-
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
@@ -28,12 +20,13 @@ const controlRecipes = async function () {
     await model.loadRecipe(id);
     const recipe = model.state.recipe;
     //Rendering recipe
-    RecipeView.render(model.state.recipe);
+    RecipeView.render(recipe);
   } catch (err) {
     alert(err);
   }
 };
 
-['hashchange', 'load'].forEach(e => {
-  window.addEventListener(e, controlRecipes);
-});
+const init = function () {
+  RecipeView.addHandlerRender(controlRecipes);
+};
+init();
