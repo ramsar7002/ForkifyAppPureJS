dfb94aed8f4f0fa0810bd6a5959e77b7593de6d0
Ram Sarfian <39989110+ramsar7002@users.noreply.github.com>
2021-08-15 15:00:42 +0300

    Update resultsView.js

--- src/js/views/resultsView.js
+++ src/js/views/resultsView.js
@@ -2,6 +2,25 @@ import View from './view.js';
 
 class ResultsView extends View {
   _parentElement = document.querySelector('.results');
+
+  _generateMarkup() {
+    console.log(this._data);
+    return `
+         <li class="preview">
+         <a class="preview__link" href="#23456">
+            <figure class="preview__fig">
+            <img src="src/img/test-1.jpg" alt="Test" />
+            </figure>
+            <div class="preview__data">
+            <h4 class="preview__name">
+               Pasta with Tomato Cream ...
+            </h4>
+            <p class="preview__publisher">The Pioneer Woman</p>
+            </div>
+         </a>
+      </li>
+     `;
+  }
 }
 
 export default new ResultsView();
