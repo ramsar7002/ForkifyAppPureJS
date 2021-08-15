60f677a62b96165d0d0dc3509c023d24fad82996
Ram Sarfian <39989110+ramsar7002@users.noreply.github.com>
2021-08-15 15:01:13 +0300

    Update view.js

--- src/js/views/view.js
+++ src/js/views/view.js
@@ -6,7 +6,6 @@ export default class View {
     this._data = data;
 
     const markup = this._generateMarkup();
-    console.log(markup);
 
     this._clear();
     this._parentElement.insertAdjacentHTML('afterbegin', markup);
