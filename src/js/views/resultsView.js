5ae011a9c805c158547464fc7e59d5535729360c
Ram Sarfian <39989110+ramsar7002@users.noreply.github.com>
2021-08-16 10:57:43 +0300

    Update resultsView.js

--- src/js/views/resultsView.js
+++ src/js/views/resultsView.js
@@ -1,25 +1,30 @@
+import icons from 'url:../../img/icons.svg';
 import View from './view.js';
 
 class ResultsView extends View {
   _parentElement = document.querySelector('.results');
+  _errorMessage = 'No recipes found for your query! Please try again';
+  _message = '';
+
+  _generateResult = item => {
+    return `
+     <li class="preview">
+            <a class="preview__link " href="#${item.id}">
+               <figure class="preview__fig">
+               <img src="${item.imageUrl}" crossorigin="anonymous" alt="${item.title}" />
+               </figure>
+               <div class="preview__data">
+               <h4 class="preview__title">${item.title}</h4>
+               <p class="preview__publisher">${item.publisher}</p>
+               </div>
+            </a>
+         </li>
+         `;
+  };
 
   _generateMarkup() {
-    console.log(this._data);
-    return `
-         <li class="preview">
-         <a class="preview__link" href="#23456">
-            <figure class="preview__fig">
-            <img src="src/img/test-1.jpg" alt="Test" />
-            </figure>
-            <div class="preview__data">
-            <h4 class="preview__name">
-               Pasta with Tomato Cream ...
-            </h4>
-            <p class="preview__publisher">The Pioneer Woman</p>
-            </div>
-         </a>
-      </li>
-     `;
+    if (this._data.length > 0)
+      return this._data.map(item => this._generateResult(item)).join('');
   }
 }
 
