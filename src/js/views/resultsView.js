0f84b09038ae1b47f6d038c73d535da08acb4d13
Ram Sarfian <39989110+ramsar7002@users.noreply.github.com>
2021-08-16 14:08:59 +0300

    Update resultsView.js

--- src/js/views/resultsView.js
+++ src/js/views/resultsView.js
@@ -7,11 +7,52 @@ class ResultsView extends View {
   _message = '';
 
   _generateResult = item => {
+    const id = window.location.hash.slice(1);
     return `
      <li class="preview">
-            <a class="preview__link " href="#${item.id}">
+            <a class="preview__link ${
+              item.id === id ? 'preview__link--active' : ''
+            }" href="#${item.id}">
                <figure class="preview__fig">
-               <img src="${item.imageUrl}" crossorigin="anonymous" alt="${item.title}" />
+               <img src="${item.imageUrl}" crossorigin="anonymous" alt="${
+      item.title
+    }" />
+               </figure>
+               <div class="preview__data">
+               <h4 class="preview__title">${item.title}</h4>
+               <p class="preview__publisher">${item.publisher}</p>
+               </div>
+            </a>
+         </li>
+         `;
+  };
+
+  _generateMarkup() {
+    if (this._data.length > 0)
+      return this._data.map(item => this._generateResult(item)).join('');
+  }
+}
+
+export default new ResultsView();
+import icons from 'url:../../img/icons.svg';
+import View from './view.js';
+
+class ResultsView extends View {
+  _parentElement = document.querySelector('.results');
+  _errorMessage = 'No recipes found for your query! Please try again';
+  _message = '';
+
+  _generateResult = item => {
+    const id = window.location.hash.slice(1);
+    return `
+     <li class="preview">
+            <a class="preview__link ${
+              item.id === id ? 'preview__link--active' : ''
+            }" href="#${item.id}">
+               <figure class="preview__fig">
+               <img src="${item.imageUrl}" crossorigin="anonymous" alt="${
+      item.title
+    }" />
                </figure>
                <div class="preview__data">
                <h4 class="preview__title">${item.title}</h4>
