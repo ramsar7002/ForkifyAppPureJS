6d1ddfb66fb58fe4ce61cb2c3404593228303e6b
Ram Sarfian <39989110+ramsar7002@users.noreply.github.com>
2021-08-16 10:59:16 +0300

    Update model.js

--- src/js/model.js
+++ src/js/model.js
@@ -1,5 +1,5 @@
 import { async } from 'regenerator-runtime';
-import { API_URL } from './config.js';
+import { API_URL, RES_PER_PAGE } from './config.js';
 import { getJSON } from './helpers.js';
 import RecipeView from './views/recipeView.js';
 
@@ -8,6 +8,8 @@ export const state = {
   search: {
     query: '',
     results: [],
+    page: 1,
+    resultsPerPage: RES_PER_PAGE,
   },
 };
 
@@ -36,18 +38,23 @@ export const loadSearchResults = async query => {
   try {
     state.search.query = query;
     const { data } = await getJSON(`${API_URL}?search=${query}`);
-    console.log(data);
     state.search.results = data.recipes.map(rec => {
       return {
         id: rec.id,
         title: rec.title,
         publisher: rec.publisher,
-        sourceUrl: rec.source_url,
+        imageUrl: rec.image_url,
       };
     });
-    console.log(state.search.results);
   } catch (err) {
     throw err;
   }
 };
-//
+
+export const getSearchResultsPage = function (page = state.search.page) {
+  state.search.page = page;
+  const start = (page - 1) * state.search.resultsPerPage;
+  const end = page * state.search.resultsPerPage;
+
+  return state.search.results.slice(start, end);
+};
