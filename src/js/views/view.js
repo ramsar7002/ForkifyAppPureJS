f53727aa7c2258a00ac6b6cdae2c561a2b28b80d
Ram Sarfian <39989110+ramsar7002@users.noreply.github.com>
2021-08-17 08:45:15 +0300

    Update view.js

--- src/js/views/view.js
+++ src/js/views/view.js
@@ -2,12 +2,13 @@ import icons from 'url:../../img/icons.svg';
 
 export default class View {
   _data;
-  render(data) {
+  render(data, render = true) {
     if (!data || (Array.isArray(data) && data.length === 0))
       return this.renderError();
     this._data = data;
     const markup = this._generateMarkup(data);
 
+    if (!render) return markup;
     this._clear();
     this._parentElement.insertAdjacentHTML('afterbegin', markup);
   }
@@ -15,13 +16,14 @@ export default class View {
   _clear() {
     this._parentElement.innerHTML = '';
   }
-  
+
   renderSpinner = () => {
     const markup = `<div class="spinner">
               <svg>
                 <use href="${icons}#icon-loader"></use>
               </svg>
             </div>`;
+
     this._clear();
     this._parentElement.insertAdjacentHTML('afterbegin', markup);
   };
