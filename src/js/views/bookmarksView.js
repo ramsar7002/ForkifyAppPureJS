715459e9db591e70f995c4be83762720feccd337
Ram Sarfian <39989110+ramsar7002@users.noreply.github.com>
2021-08-17 08:43:33 +0300

    Create bookmarksView.js

---
+++ src/js/views/bookmarksView.js
@@ -0,0 +1,17 @@
+import PreviewView from './previewView';
+import View from './view';
+
+class BookmarksView extends View {
+  _parentElement = document.querySelector('.bookmarks__list');
+  _errorMessage = 'No bookmarks yet. Find a nice recipe and bookmark it!';
+  _message = '';
+
+  _generateMarkup() {
+    if (this._data.length > 0)
+      return this._data
+        .map(bookmark => PreviewView.render(bookmark, false))
+        .join('');
+  }
+}
+
+export default new BookmarksView();
